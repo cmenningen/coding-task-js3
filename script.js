8670ef5943a374986815e2f4b1bf4b80f0e4fdb3
Christoph Menningen <christoph.menningen@gmail.com>
2024-01-09 15:16:46 +0100

    test commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-# coding-task-js3
+// TODO: implement welcomeMsg function
+function welcomeMsg(a, b) {
+  console.log(a + b);
+}
+
+welcomeMsg("Welcome", " Mary");
+welcomeMsg("Welcome", " Karl-Heinz");
+
+// TODO: implement calcGrossPrice function
+function calcGrossPrize(a, b) {
+  console.log(a + a * b);
+}
+
+calcGrossPrize(20, 0.19);
+calcGrossPrize(40, 0.16);
+
+// TODO: Implement the addPositive function
+function addPositive(a, b) {
+  console.log(Math.abs(a) + Math.abs(b));
+}
+
+addPositive(2, 3);
+addPositive(3, -5);
+addPositive(-1, -8);
